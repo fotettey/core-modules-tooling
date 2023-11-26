40792c2dab38f0c3c78ec001684fb60eed912a68
fotettey <fotettey@gmail.com>
2023-11-26 16:03:20 +0100

    minor updates

--- gcp-core/private-subnetworks/variables.tf
+++ gcp-core/private-subnetworks/variables.tf
@@ -23,14 +23,6 @@ variable "sa_path" {
   default = "/home/vagrant/projects/.secrets/metro-infra-dev-4e86d7cc9bda.json"
 }
 
-variable "private_subnet3" {
-        default = "10.0.3.0/24"
-    }
-
-variable "public_subnet3" {
-        default = "10.0.103.0/24"
-    }
-
 ######
 
 variable "public_cidrs" {}
--- gcp-core/public-subnetworks/subnetworks.tf
+++ gcp-core/public-subnetworks/subnetworks.tf
@@ -5,7 +5,8 @@ data "google_compute_network" "network" {
 resource "google_compute_subnetwork" "public_subnet" {
     #provider = google-beta
   name          =  "${var.project}-public"
-  ip_cidr_range = var.public_cidrs
+  for_each = toset( [var.public_cidrs] )
+  ip_cidr_range = each.key
   network       = data.google_compute_network.network.name
   region        = var.region
    # count and for_each ARGS cannot be used together, as they can be used to set resource number.connection {
