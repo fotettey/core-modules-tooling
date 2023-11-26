12e4c316c391e9ec9583ef9f9c1ecd7ce80579a0
fotettey <fotettey@gmail.com>
2023-11-26 14:10:48 +0100

    removed count ARG from subnet resource blocks to avert error

--- gcp-core/public-subnetworks/subnetworks.tf
+++ gcp-core/public-subnetworks/subnetworks.tf
@@ -8,8 +8,11 @@ resource "google_compute_subnetwork" "public_subnet" {
   ip_cidr_range = var.public_cidrs
   network       = data.google_compute_network.network.name
   region        = var.region
+   # count and for_each ARGS cannot be used together, as they can be used to set resource number.connection {
+   # Using both leads to error: The "count" and "for_each" meta-arguments are mutually-exclusive, only one should be used to be explicit about the number of
+   # resources to be created.
+   # count = var.pub_subnet_count
   project = var.project
-  count = var.pub_subnet_count
   depends_on = [ data.google_compute_network.network ]
 }
 
