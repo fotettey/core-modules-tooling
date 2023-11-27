4948dbfb8bef90ac8dc5f3ea6adde6c14cf1bc92
fotettey <fotettey@gmail.com>
2023-11-27 02:17:25 +0100

    reset cidr var change

--- gcp-core/private-vpc/variables.tf
+++ gcp-core/private-vpc/variables.tf
@@ -23,9 +23,13 @@ variable "sa_path" {
   default = "/home/vagrant/projects/.secrets/metro-infra-dev-4e86d7cc9bda.json"
 }
 
-variable "public_cidrs" {
-    default = {public_subnet1 = "10.0.101.0/24", public_subnet2 = "10.0.102.0/24", public_subnet3 = "10.0.103.0/24"}
-}
-variable "private_cidrs" {
-    default = {private_subnet1 = "10.0.1.0/24", private_subnet2 = "10.0.2.0/24", private_subnet3 = "10.0.3.0/24"}
-}
+# variable "public_cidrs" {
+#     default = {public_subnet1 = "10.0.101.0/24", public_subnet2 = "10.0.102.0/24", public_subnet3 = "10.0.103.0/24"}
+# }
+# variable "private_cidrs" {
+#     default = {private_subnet1 = "10.0.1.0/24", private_subnet2 = "10.0.2.0/24", private_subnet3 = "10.0.3.0/24"}
+# }
+
+
+variable "public_cidrs" {}
+variable "private_cidrs" {}
